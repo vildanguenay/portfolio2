fec9cf783e1b2d676ce8e4d905e6a5663e4c84fb
unknown <vildan-guenay@hotmail.com>
2024-01-28 15:41:35 +0100

    remove banner image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -78,7 +78,7 @@ export const Banner = () => {
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <img src={headerImg} alt="Header Img" />
+            {/* <img src={headerImg} alt="Header Img" /> */}
           </Col>
         </Row>
       </Container>
